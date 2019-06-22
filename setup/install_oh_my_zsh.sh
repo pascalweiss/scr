d5ea1522544422931caf52f3845b80072005ada3
Pascal Weiß <pa-weiss@gmx.de>
2019-06-22 17:42:38 +0200

    Fixed broken oh-my-zsh installation

--- setup/install_oh_my_zsh.sh
+++ setup/install_oh_my_zsh.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 function install_packages () {
-    os_package_install "zsh"
     curl -Lo oh_my_install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
     sh oh_my_install.sh --unattended
     rm oh_my_install.sh
